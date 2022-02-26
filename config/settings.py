0b4f7beeafaf3c7e4e5b09304522f68478ec41e9
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2022-02-26 23:59:00 +0300

    implements postgresql database configuration [Start #181361466]

--- config/settings.py
+++ config/settings.py
@@ -69,8 +69,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT')
     }
 }
 
