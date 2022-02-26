f854b922bf45c8e132bbf5bc77c3083b66720edb
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2022-02-26 21:49:45 +0300

    fix allowed host heroku configurations

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'twin-brook-system.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
